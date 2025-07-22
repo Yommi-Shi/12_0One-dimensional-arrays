4c6c38b07da8d7f571dc21da5cdc759dd3355af8
Evgenii Blinov <scythian89@gmail.com>
2025-07-22 22:00:12 +0300

    homework#12_1

---
+++ 01_apartment-building/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+
+int main() {
+    std::cout << "===Apartment building===" << std::endl;
+
+    std::cout << "Enter 10 surname" << std::endl;
+    std::string surname[10];
+
+    for (int i = 0; i < 10; i++) {
+        std::cin >> surname[i];
+    }
+
+    std::cout << "Enter three apartment numbers" << std::endl;
+
+    int number;
+    for (int i = 0; i < 3; i++) {
+        std::cin >> number;
+        if (number > 10) {
+            std::cout << "Incorrect input.Apartment number from 1 to 10" << std::endl;
+        } else {
+            std::cout << surname[number - 1] << std::endl;
+        }
+    }
+}
