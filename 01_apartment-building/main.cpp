d81a0b5b2dba072be58fda469c3e0fb1b57b9c4e
Evgenii Blinov <scythian89@gmail.com>
2025-07-24 19:11:58 +0300

    homework#12_1_adding a check

--- 01_apartment-building/main.cpp
+++ 01_apartment-building/main.cpp
@@ -17,7 +17,7 @@ int main() {
     int number;
     for (int i = 0; i < 3; i++) {
         std::cin >> number;
-        if (number > 10) {
+        if (number > 10 || number <= 0) {
             std::cout << "Incorrect input.Apartment number from 1 to 10" << std::endl;
         } else {
             std::cout << surname[number - 1] << std::endl;
