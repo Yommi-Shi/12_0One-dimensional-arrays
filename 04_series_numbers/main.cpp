acbb6ffa5686890301c56cad302c7a6073614b54
Evgenii Blinov <scythian89@gmail.com>
2025-07-25 20:57:28 +0300

    12.4 Series_of_numbers

---
+++ 04_series_numbers/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+int main() {
+    std::cout << "===Series of numbers===" << std::endl;
+
+    int numbers[15];
+    int size = 15;
+    int repeat_sum = 0;
+    int sum = 0;
+
+    std::cout << "Enter a set of 14 consecutive numbers and one number from the series is repeated:" << std::endl;
+
+    for (int i = 0; i < size; i++) {
+        std::cin >> numbers[i];
+    }
+
+    int x = numbers[0];
+
+    for (int i = 1; i < size; i++) {
+        if (numbers[i] < x) {
+            x = numbers[i];
+        }
+    }
+
+    int max_number = x + size - 1;
+
+    for (int i = 0; i < size; i++) {
+        repeat_sum += numbers[i];
+        sum += x;
+        x++;
+    }
+
+    std::cout << "Repeating number: " << max_number - (sum - repeat_sum);
+
+}
